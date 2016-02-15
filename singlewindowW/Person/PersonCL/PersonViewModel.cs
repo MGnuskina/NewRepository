931f28c1ccbdea1e594b63a20e6fe3f0d02ea76e
MGnuskina <mariya.gnuskina@gmail.com>
2016-02-15 13:43:40 +0200

    new rep
    new try

---
+++ singlewindowW/Person/PersonCL/PersonViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace PService
+{
+    public class PersonViewModel
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        [Range(0,120)]
+        public int Age { get; set; }
+
+        //check that it is a phone number!/
+        public List<PhoneNumberViewModel> PhoneNumbers { get; set; }
+
+        //public enum PNTypes ///maybe not here.........
+        //{
+        //    Mobile,
+        //    Work,
+        //    Home
+        //}
+    }
+}
